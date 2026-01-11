7399b9c3202176e052e2afb533a0d4a094780cab
Jelle228 <jvanschoonenberghe1@gmail.com>
2026-01-11 15:50:25 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => (
-  <div
-  style={{
-    width: 280,
-    height: 420,
-    border: "2px dashed #bbb",
-    borderRadius: 16,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 16,
-    color: "#777",
-  }}
->
-  Character Preview
-</div>
-<div style={{ padding: 24 }}>
-    <h1>Anime Character Lab</h1>
-    <p>Project setup successful ✅</p>
+  <div>
+    <div
+      style={{
+        width: 280,
+        height: 420,
+        border: "2px dashed #bbb",
+        borderRadius: 16,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginBottom: 16,
+        color: "#777"
+      }}
+    >
+      Character Preview
+    </div>
+
+    <div style={{ padding: 24 }}>
+      <h1>Anime Character Lab</h1>
+      <p>Project setup successful ✅</p>
+    </div>
   </div>
 );
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
